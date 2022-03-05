e89e6a9a2fec6bf6706ac9f9570ab210166afda1
yamank41 <100734289+yamank41@users.noreply.github.com>
2022-03-05 13:20:52 +0300

    حطيت الشخصيات

--- ios-cw-3/ios-cw-3/ContentView.swift
+++ ios-cw-3/ios-cw-3/ContentView.swift
@@ -2,15 +2,43 @@
 //  ContentView.swift
 //  ios-cw-3
 //
-//  Created by Retaj Al-Otaibi on 04/03/2022.
+//  Created by Yaman Koujan on 05/03/2022.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+    struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("ll")
+                .resizable()
+                .frame(width:500, height:1000)
+            VStack{
+                Text("these are a good charecters")
+                    .padding()
+//                Spacer()
+                HStack{Image("hh")
+                        .resizable()
+                        .frame(width:140, height:140)
+                    Image("bb")
+                        .resizable()
+                        .frame(width:140, height:140)}
+                HStack{Image("cc")
+                        .resizable()
+                        .frame(width:140, height:140)
+                    
+                    Image("vv")
+                        .resizable()
+                        .frame(width:140, height:140)
+                    Image("tt")
+                        .resizable()
+                        .frame(width:140, height:140)}.padding()
+                
+
+    
+        }
+        
+        }
     }
 }
 
